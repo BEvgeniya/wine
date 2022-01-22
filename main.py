283ad17f981cb229f524f3797e9d70f46d731789
BEvgeniya <evgeniyaj88@gmail.com>
2022-01-23 03:31:54 +0500

    шаг 17

--- main.py
+++ main.py
@@ -1,44 +1,36 @@
 import datetime
+import pandas
 
+from collections import defaultdict
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-import pandas
-from pprint import pprint
-from itertools import groupby
-from pprint import pprint
-from collections import defaultdict
+
 
 def isNaN(num):
     return num != num
-    
+
+
 event1 = datetime.datetime(year=1920,
-   month=12, day=24)
+                           month=12, day=24)
 event2 = datetime.datetime.now()
 agewine = event2 - event1
 
 event1_year = event1.year
 event2_year = event2.year
-agewine_year = event2_year-event1_year
+agewine_year = event2_year - event1_year
 
 excel_data_df = pandas.read_excel('wine3.xlsx', sheet_name='Лист1')
 wines = excel_data_df.to_dict(orient='record')
 
-#grouped_wines = {}
 grouped_wines = defaultdict(list)
 for wine in wines:
     for category in wine:
         if isNaN(wine[category]):
             wine[category] = ''
-
-#    if grouped_wines.get(wine['Категория']) == None:
-#        grouped_wines[wine['Категория']] = [wine]
-#    else:
-#        grouped_wines[wine['Категория']].append(wine)
     grouped_wines[wine['Категория']].append(wine)
 
 sorted_items = sorted(grouped_wines.items(), key=lambda x: x[0])
 grouped_wines = dict(sorted_items)
-pprint(grouped_wines)
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -53,6 +45,6 @@ rendered_page = template.render(
 
 with open('index.html', 'w', encoding="utf8") as file:
     file.write(rendered_page)
-    
+
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
 server.serve_forever()
